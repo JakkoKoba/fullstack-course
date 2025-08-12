98ce9fb92a3fa03a65b15450aa986984abbe7cde
Jakko Koba <jakub.pk.koba@gmail.com>
2025-08-12 20:29:01 +0200

    Complete exercise 1.7 of part 1

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -3,22 +3,56 @@ import { useState } from 'react'
 const Button = (props) => <button onClick={props.action}> {props.text} </button>
 const Display = (props) => <h1> {props.text} </h1>
 
-const Total = (props) => (
+const List = (props) => (
   <>
-    <p>good {props.good} <br/>
+    good {props.good} <br/>
     neutral {props.neutral} <br/>
-    bad {props.bad}</p>
+    bad {props.bad} <br/>
+    all {props.total} <br/>
   </>
 )
+
+const AvgScore = (props) => {
+  const avg = props.score / props.total
+  return <>average {avg}</>
+}
+
+const Positive = (props) => {
+  const positive = props.good / props.total * 100
+  return <>positive {positive} %</>
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const isGood = () => setGood(good + 1)
-  const isNeutral = () => setNeutral(neutral + 1)
-  const isBad = () => setBad(bad + 1)
+  const [score, setScore] = useState(0)
+
+  const isGood = () => {
+    setScore(score + 1)
+    setTotal(total + 1)
+    return (
+      setGood(good + 1)
+    )
+  }
+
+  const isNeutral = () => {
+    setScore(score + 0)
+    setTotal(total + 1)
+    return (
+      setNeutral(neutral + 1)
+    )
+  }
+
+  const isBad = () => {
+    setScore(score - 1)
+    setTotal(total + 1)
+    return (
+      setBad(bad + 1)
+    )
+  }
 
   return (
     <div>
@@ -27,7 +61,9 @@ const App = () => {
       <Button text="neutral" action={isNeutral}/>
       <Button text="bad" action={isBad}/>
       <Display text="statistics"/>
-      <Total good={good} neutral={neutral} bad={bad}/>
+      <List good={good} neutral={neutral} bad={bad} total={total}/>
+      <AvgScore score={score} total={total}/> <br/>
+      <Positive good={good} total={total}/>
     </div>
   )
 }
