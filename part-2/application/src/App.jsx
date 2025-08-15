61d676b35a71ecfc1c3f78ac19ae0f1943bb3261
Jakko Koba <jakub.pk.koba@gmail.com>
2025-08-15 13:44:55 +0200

    Complete exercise 2.1 of part 2
    Update name of note application to "note"

--- part-2/application/src/App.jsx
+++ part-2/application/src/App.jsx
@@ -1,17 +1,52 @@
-import Note from './components/Note'
+const Course = (props) => (
+  <>
+    <Header course={props.course.name} />
+    <Content parts={props.course.parts} />
+  </>
+)
 
-const App = ({ notes }) => {
+const Header = (props) => <h1>{props.course}</h1>
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note =>
-          <Note key={note.id} note={note}/>
-        )}
-      </ul>
-    </div>
-  )
+const Content = (props) => (
+  <div>
+    {props.parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
